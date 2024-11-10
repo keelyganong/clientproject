c08b1b8e92658579e81be8fd84b77bcd7bef8ec8
Keely Ganong <keelyganong@Keelys-MacBook-Pro-5.local>
2024-11-10 14:33:23 -0500

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     document.querySelector('nav').classList.toggle('active');
+//   });
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("show");
+}
